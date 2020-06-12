6e9ea8aba50b3547394182a9d53de39c6504988a
Salt-Admin <salt-admin@Salt-Admins-MacBook-Pro.local>
2020-06-12 18:59:18 +0200

    Functionality added to ChatUserList component

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,10 +4,12 @@ import { User } from './actions/actions.user.types';
 import thunk from 'redux-thunk';
 import socketMiddleware from './middlewares/middleware.socket';
 import { Message } from './actions/actions.messages.types';
+import { Chatuser } from './actions/actions.chatusers.types';
 
 export interface RootState {
   user: { data: User };
   messages: { data: Message[] };
+  chatusers: { data: Chatuser[] };
 }
 
 const composeEnhancers =
