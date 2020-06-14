2ad27e2785575789cfc7f9ccfafb068da8fc4bdb
Salt-Admin <salt-admin@Salt-Admins-MacBook-Pro.local>
2020-06-14 08:07:50 +0200

    Theme moved to config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import { createMuiTheme } from '@material-ui/core';
+
 const prod: { ROOT_URL: string } = {
   ROOT_URL: 'https://lets-get-chatty.herokuapp.com/',
 };
@@ -6,3 +8,16 @@ const dev: { ROOT_URL: string } = {
 };
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#fff',
+      main: 'rgb(23, 105, 170)',
+      dark: '#000',
+    },
+    secondary: {
+      main: '#58a5f0',
+    },
+  },
+});
